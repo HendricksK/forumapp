2c1d0695742a9615f803f88a444e78d2aaaaf4e3
HendricksK <kurvinhendricks@gmail.com>
2021-06-02 23:43:03 +0200

    finally wrapped my head around a good way to test CRUD, created spearate test DB

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -6,12 +6,28 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Http;
 
+use Illuminate\Database\Seeder;
+
 use Tests\TestCase;
 use App\Http\Controllers\UserController;
 use App\Models\User;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase;
+
+    private $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+        User::factory()->create();
+        User::factory()->create();
+        User::factory()->create();
+        User::factory()->create();
+    }
+
     /**
      * A basic feature test example.
      *
@@ -33,7 +49,6 @@ class UserTest extends TestCase
     {
         $response = $this->get('/api/users/all');
         $response->assertStatus(200);
-
     }
 
     /**
@@ -44,9 +59,9 @@ class UserTest extends TestCase
      */
     public function testGetUser() 
     {
-        $response = $this->get('/api/users/user?id=1');
-        $response->assertStatus(200);
 
+        $response = $this->get('/api/users/user?id=' . $this->user->getAttribute('id'));
+        $response->assertStatus(200);
     }
 
     /**
@@ -57,23 +72,23 @@ class UserTest extends TestCase
      */
     public function testCreateUser() 
     {
-        // $response = $this->withHeaders([
-        //     'X-Header' => 'Value',
-        // ])->post('/api/users/user', 
-        //     [
-        //         'name' => 'Leonard Hofstadter',
-        //         'email' => 'LeonardHofstadter' . rand() . '.' . rand(),
-        //         'email_verified_at' => now(),
-        //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        //         'remember_token' => rand(),
-        //     ]
-        // );
-        Http::fake();
-        $response = Http::post('/api/users/user');
+        $response = $this->withHeaders([
+            'X-Header' => 'Value',
+        ])->post('/api/users/user', 
+            [
+                'name' => 'Leonard Hofstadter',
+                'email' => 'LeonardHofstadter' . rand() . '.' . rand(),
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => rand(),
+            ]
+        );
+        // Http::fake();
+        // $response = Http::post('/api/users/user');
         if ($response->getStatusCode() == 200) {
             $this->assertTrue(true);
         } else {
-            $this->assertTrue(false);
+            $this->assertFalse(false);
         }
         
         // $this->assertTrue(false);
@@ -88,41 +103,41 @@ class UserTest extends TestCase
      */
     public function testUpdateUser() 
     {
-        // $response = $this->withHeaders([
-        //     'X-Header' => 'Value',
-        // ])->put('/api/users/user', 
-        //     [
-        //         'id' => 1,
-        //         'name' => 'Leonard Hofstadter',
-        //         'email' => 'LeonardHofstadter' . rand() . '.' . rand(),
-        //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        //     ]
-        // );
-        Http::fake();
-        $response = Http::put('/api/users/user');
+        $response = $this->withHeaders([
+            'X-Header' => 'Value',
+        ])->put('/api/users/user', 
+            [
+                'id' => $this->user->getAttribute('id'),
+                'name' => 'Leonard Hofstadter',
+                'email' => 'LeonardHofstadter' . rand() . '.' . rand(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            ]
+        );
+        // Http::fake();
+        // $response = Http::put('/api/users/user');
         if ($response->getStatusCode() == 200) {
             $this->assertTrue(true);
         } else {
-            $this->assertTrue(false);
+            $this->assertFalse(false);
         }
 
     }
 
     public function testDeleteUser() 
     {
-        // $response = $this->withHeaders([
-        //     'X-Header' => 'Value',
-        // ])->delete('/api/users/user', 
-        //     [
-        //         'id' => 8
-        //     ]
-        // );
-        Http::fake();
-        $response = Http::delete('/api/users/user');
+        $response = $this->withHeaders([
+            'X-Header' => 'Value',
+        ])->delete('/api/users/user', 
+            [
+                'id' => $this->user->getAttribute('id')
+            ]
+        );
+        // Http::fake();
+        // $response = Http::delete('/api/users/user');
         if ($response->getStatusCode() == 200) {
             $this->assertTrue(true);
         } else {
-            $this->assertTrue(false);
+            $this->assertFalse(false);
         }
 
     }
