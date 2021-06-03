cb37f95bf992ac038daddf2cea4ac4e0915d7a47
HendricksK <kurvinhendricks@gmail.com>
2021-06-03 19:00:48 +0200

    custom log file and logger

--- app/Http/Middleware/ApiLogger.php
+++ app/Http/Middleware/ApiLogger.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\ApiLoggerController;
 
 class ApiLogger
@@ -20,6 +21,8 @@ class ApiLogger
         $logger = new ApiLoggerController();
         $response = $next($request);
         $logger->log($request, $response);
+        
+        Log::channel('api')->info($request . $response);
 
         return $response;
     }
